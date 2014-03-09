5899ba8786c42ac638fddd9024cbe7f8e5120331
Jon E Rankin <jr0436@bris.ac.uk>
2014-03-09 05:59:15 +0000

    starting on multi-slide in

---
+++ arx/music.js
@@ -0,0 +1,126 @@
+
+            var functionIsFree = 1;
+
+            
+            $('#reflektor').click(function(e){
+                    if(e.handled !== true){
+
+
+                    if($('.drawer').is(':hidden')){
+                        var div_height = $('#audio').height() + 400;
+
+                        $('#audio').animate({
+                        height: div_height}, 200); 
+                        $('#slidereflektor').toggleClass('hideB'); 
+
+                        window.setTimeout(function(){ $('.drawer').toggleClass('active');},400);
+                    }
+                    else if($('#slidereflektor').is(':visible')){
+                         var div_height = $('#audio').height() - 400;
+                         $('#slidereflektor').toggleClass('active');
+                         $('#slidereflektor').toggleClass('hideB');
+                        $('#audio').animate({
+                            height: div_height}, 200);
+                        }
+
+
+
+                      e.preventDefault(); 
+                }
+                e.handled = true;
+            });     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            $(".info").click((function() {
+                var i = 0;
+                return function() {
+                  /*  if($('#audio').height() == $(window).height()){
+                         $('#audio').animate({
+                       height: '60%'}, 1);
+                    }else{
+                    $('#audio').animate({
+                       height: '100%'}, 2);
+                }*/
+
+
+                     var div_height = $('#audio').height();
+                    var vertical_div_top_position = $('#audio').offset().top;
+                    var lastchild_height = $('.spotify').height();
+                    var vertical_lastchild_top_position = $('.spotify').offset().top;
+                    var vertical_lastchild_bottom_position = lastchild_height + vertical_lastchild_top_position;
+                    var real_height = vertical_lastchild_bottom_position - vertical_div_top_position;
+                    console.log("spotify:"+ real_height + " div:" + div_height);
+                    console.log($(window).height());
+                    word="";
+                    j=100;
+
+                    while (real_height > div_height){
+                        word=j+"%";
+                         $('#audio').css("height", word);
+                    div_height = $('#audio').height();
+                    vertical_div_top_position = $('#audio').offset().top;
+                    lastchild_height = $('.spotify').height();
+                    vertical_lastchild_top_position = $('.spotify').offset().top;
+                    vertical_lastchild_bottom_position = lastchild_height + vertical_lastchild_top_position;
+                    real_height = vertical_lastchild_bottom_position - vertical_div_top_position;
+                         j=j+5;
+    //overflow div
+                        }
+
+
+
+
+console.log($('#audio').height())
+
+
+
+
+
+
+                }
+            })());
+
+
+       /*     window.onresize = function() {
+
+                  var div_height = $('#audio').height();
+                    var vertical_div_top_position = $('#audio').offset().top;
+                    var lastchild_height = $('.spotify').height();
+                    var vertical_lastchild_top_position = $('.spotify').offset().top;
+                    var vertical_lastchild_bottom_position = lastchild_height + vertical_lastchild_top_position;
+                    var real_height = vertical_lastchild_bottom_position - vertical_div_top_position;
+                    console.log("spotify:"+ real_height + " div:" + div_height);
+                    console.log($(window).height());
+                    word="";
+                    j=100;
+
+                    while (real_height > div_height){
+                        word=j+"%";
+                         $('#audio').css("height", word);
+                      div_height = $('#audio').height();
+                      vertical_div_top_position = $('#audio').offset().top;
+                      lastchild_height = $('.spotify').height();
+                      vertical_lastchild_top_position = $('.spotify').offset().top;
+                      vertical_lastchild_bottom_position = lastchild_height + vertical_lastchild_top_position;
+                      real_height = vertical_lastchild_bottom_position - vertical_div_top_position;
+                      j=j+5;
+    //overflow div
+                        }
+
+                }
+
+*/
